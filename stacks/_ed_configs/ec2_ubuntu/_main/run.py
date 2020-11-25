40138a09f3592b9d783d473e3b313544a8165a6a
Gary <gary@elasticdev.io>
2020-11-25 14:29:20 +0800

    update

--- stacks/_ed_configs/ec2_ubuntu/_main/run.py
+++ stacks/_ed_configs/ec2_ubuntu/_main/run.py
@@ -12,8 +12,16 @@ def run(stackargs):
     # Add default variables
     stack.parse.add_required(key="hostname")
     stack.parse.add_required(key="keyname")
-    stack.parse.add_required(key="aws_default_region",default="null")
+    stack.parse.add_required(key="aws_default_region",default="us-east-1")
     stack.parse.add_required(key="region",default="null")
+
+    stack.parse.add_optional(key="vpc_name",default="null")
+    stack.parse.add_optional(key="vpc_id",default="null")
+    stack.parse.add_optional(key="security_groups",default="null")
+    stack.parse.add_optional(key="security_groups_ids",default="null")
+    stack.parse.add_optional(key="subnet",default="null")
+    stack.parse.add_optional(key="subnet_id",default="null")
+
     stack.parse.add_optional(key="image",default="null")
     stack.parse.add_optional(key="image_name",default="null")
     stack.parse.add_optional(key="image_ref",default="elasticdev:::public::ubuntu.16.04-chef_solo")
@@ -36,18 +44,25 @@ def run(stackargs):
     default_values["size"] = "t2.micro"
     default_values["disksize"] = 40
     default_values["timeout"] = 600
-    default_values["security_group"] = "default"
-    default_values["placement"] = None
-    default_values["vpc"] = None
-    default_values["vpc_label"] = None
-    default_values["sg"] = None
-    default_values["sg_label"] = None
     default_values["tags"] = None
-    default_values["comment"] = None
     default_values["image_ref"] = stack.image_ref
+    default_values["aws_default_region"] = stack.aws_default_region
+
+    # vpc
+    if stack.vpc_name: default_values["vpc_name"] = stack.vpc_name
+    if stack.vpc_id: default_values["vpc_id"] = stack.vpc_id
+
+    # subnet
+    if stack.subnet: default_values["subnet"] = stack.subnet
+    if stack.subnet_id: default_values["subnet_id"] = stack.subnet_id
+
+    # security groups
+    if stack.security_groups: default_values["security_groups"] = stack.security_groups
+    if stack.security_groups_ids: default_values["security_groups_ids"] = stack.security_groups_ids
+
+    # ami image
     if stack.image: default_values["image"] = stack.image
     if stack.image_name: default_values["image_name"] = stack.image_name
-    if stack.aws_default_region: default_values["aws_default_region"] = stack.aws_default_region
     if stack.region: default_values["region"] = stack.region
 
     inputargs = {"default_values":default_values}
@@ -56,8 +71,7 @@ def run(stackargs):
     stack.ec2_server.insert(display=None,**inputargs)
 
     # Call to bootstrap_ed to ed
-    default_values = {}
-    default_values["hostname"] = stack.hostname
+    default_values = {"hostname":stack.hostname}
     default_values["keyname"] = stack.keyname
     default_values["ip_key"] = "private_ip"
     default_values["user"] = "ubuntu"
